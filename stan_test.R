cf60ef92b6e358f14cfc606fb69b05246f37d800
esq601 <esq601@gmail.com>
2022-04-18 17:04:05 -0700

    initial commit

---
+++ stan_test.R
@@ -0,0 +1,26 @@
+library(rstan)
+library(tidyverse)
+
+
+rstan::get_posterior_mean(fit)
+
+
+lambda <- 1
+dat <- rexp(500, lambda)
+
+len <- length(dat)
+
+fit <- stan(
+  file = "stan_test.stan",
+  data = list(Y=dat, LENGTH = len),
+  warmup = 750,
+  iter = 1500,
+  chains = 3
+  
+)
+
+print(fit)
+
+mcmc_chain = as.matrix(fit)
+
+plot(density(mcmc_chain[,'lambda']))
