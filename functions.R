ede36d1257635cb25da4ffb66089d94c16d5bf29
esq601 <esq601@gmail.com>
2022-04-25 14:39:41 -0700

    not working!

--- functions.R
+++ functions.R
@@ -49,26 +49,69 @@ R_g <- function(j,L,W,t,tau,lambda,alpha,beta){
 pdf_R_g <- function(j,L,W,t,tau,lambda,alpha,beta){
  
   p <- 0
+  m <- L*W
   
   if(t <= tau) {
     for(i in 0:j) {
-      p_temp <- faults(i,L,W) * (i*lambda*((1-exp(-t*lambda))^(i-1))*exp(lambda*t*(-(m-i))-lambda*t)) - (lambda*(m-i)*((1-exp(-t*lamba))^i) * exp(lambda*t*(-(m-a))))
+      p_temp <- faults(i,L,W) * -((i*lambda*((1-exp(-t*lambda))^(i-1))*exp(lambda*t*(-(m-i))-lambda*t)) - (lambda*(m-i)*((1-exp(-t*lambda))^i) * exp(lambda*t*(-(m-i)))))
+      print(p_temp)
       p <- p + p_temp
     }
     
   } else {
     for(i in 0:j) {
-      p_temp <- faults(i,L,W) * (i*(m-i)*(((t-tau)/beta)^(i-1))*((1-exp(-lambda*tau-((t-tau)/beta)^alpha))^i)*exp(-(m-i)*((t-tau)/beta)^alpha))/beta - (i*alpha*(((t-tau)/beta)^(alpha-1))*((1-exp(-lambda*tau-((t-tau)/beta)^alpha))^(i-1))*exp(-(m-i)*(((t-tau)/beta)^alpha)-lambda*tau-(((t-tau)/beta)^alpha))/beta
+      # p_temp <- faults(i,L,W) * 
+      #   ((alpha*(m-i)*(((t-tau)/beta)^(alpha-1))*((1-exp(-lambda*tau-((t-tau)/beta)^alpha))^i)*exp(-(m-i)*lambda*tau+((t-tau)/beta)^alpha))/beta - ((alpha*i*(((t-tau)/beta)^(alpha-1)))*((1-exp(-lambda*tau-((t-tau)/beta)^alpha))^(i-1))*exp(-(m-i)*(lambda*tau+((t-tau)/beta)^alpha)-lambda*tau-(((t-tau)/beta)^alpha)))/beta)
       
+      p_temp <- faults(i,L,W) * -(-(alpha*i*firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^(i-1)*(secondpart(lambda,tau,t,alpha,beta))^(-i+m+1))/beta -
+        (alpha*i*firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^(i)*(secondpart(lambda,tau,t,alpha,beta))^(m-i))/beta +
+        (alpha*m*firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^(i)*(secondpart(lambda,tau,t,alpha,beta))^(-i+m+1))/beta)
+      p <- p + p_temp
     }
   }
+  p
    
 }
 
-R_g(2,4,2,10,20,.0024,10,10)
+
+
+firstpart <- function(t,tau,beta,alpha){
+  ((t-tau)/beta)^(alpha-1)
+}
+
+secondpart <- function(lambda,tau,t,alpha,beta){
+  exp(-lambda*tau-((t-tau)/beta)^alpha)
+}
 
 
 
+
+
+pdf_R_g(2,4,2,10,20,.0024,10,10)
+
+alpha <- 5
+beta <- 55
+tau <- 20
+lambda <- .0024
+L <- 400
+W <- 2
+j <- 200
+
+df <- data.frame() 
+
+for(i in seq(from =1,to =40, by = .1)) {
+  
+  df <- bind_rows(df,
+                  data.frame(t=i,
+                             rc = R_c(t=i,tau=tau, alpha = alpha, beta = beta),
+                             R_g = R_g(j,L,W,t=i,tau,lambda,alpha,beta),
+                             f_t = pdf_R_g(j,L,W,t=i,tau,lambda,alpha,beta)))
+  
+}
+
+ggplot(df, aes(x = t, y = f_t)) +
+  geom_path() +
+  geom_path(aes(y = 1-R_g),color = "red")
 
 alp <- 3
 bet <- 20
@@ -79,8 +122,6 @@ for(i in seq(from =1,to = 50, by = .1)) {
   df <- bind_rows(df,data.frame(t=i,rc = R_c(t=i,tau=tau, alpha = alp, beta = bet),R_g = R_g(64,120,2,t=i,tau,.0024,alp,bet)))
     
 }
-
-
 
 
 
