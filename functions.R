6383d1ebe7a45d80b0c44918bac43db94c90ac13
esq601 <esq601@gmail.com>
2022-04-25 15:39:26 -0700

    it works

--- functions.R
+++ functions.R
@@ -13,21 +13,21 @@ ggplot(samples)+
 
 R_c <- function(t=0,tau=20,lambda=.0024,alpha=1,beta=1) {
   if(t <= tau) {
-    #print("no heyoo")
+    
     exp(-lambda*t)
   } else {
-    #print("heyoo")
-    exp(-(lambda*t + ((t-tau)/beta)^alpha))
+    
+    exp(-(lambda*tau + ((t-tau)/beta)^alpha))
   }
 }
 
 faults <- function(i,L,W) {
   f <- 0
-    #print(floor(i/W))
+  
     for(k in 0:floor(i/W)){
-      #print(k)
+      
       f_temp <- ((-1)^k)*(choose(L*W-k*W,i-k*W))*(choose(L,k))
-      #print(f_temp)
+      
       f <- f + f_temp
     }
   f
@@ -45,36 +45,6 @@ R_g <- function(j,L,W,t,tau,lambda,alpha,beta){
   rel
 }
 
-
-pdf_R_g <- function(j,L,W,t,tau,lambda,alpha,beta){
- 
-  p <- 0
-  m <- L*W
-  
-  if(t <= tau) {
-    for(i in 0:j) {
-      p_temp <- faults(i,L,W) * -((i*lambda*((1-exp(-t*lambda))^(i-1))*exp(lambda*t*(-(m-i))-lambda*t)) - (lambda*(m-i)*((1-exp(-t*lambda))^i) * exp(lambda*t*(-(m-i)))))
-      print(p_temp)
-      p <- p + p_temp
-    }
-    
-  } else {
-    for(i in 0:j) {
-      # p_temp <- faults(i,L,W) * 
-      #   ((alpha*(m-i)*(((t-tau)/beta)^(alpha-1))*((1-exp(-lambda*tau-((t-tau)/beta)^alpha))^i)*exp(-(m-i)*lambda*tau+((t-tau)/beta)^alpha))/beta - ((alpha*i*(((t-tau)/beta)^(alpha-1)))*((1-exp(-lambda*tau-((t-tau)/beta)^alpha))^(i-1))*exp(-(m-i)*(lambda*tau+((t-tau)/beta)^alpha)-lambda*tau-(((t-tau)/beta)^alpha)))/beta)
-      
-      p_temp <- faults(i,L,W) * -(-(alpha*i*firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^(i-1)*(secondpart(lambda,tau,t,alpha,beta))^(-i+m+1))/beta -
-        (alpha*i*firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^(i)*(secondpart(lambda,tau,t,alpha,beta))^(m-i))/beta +
-        (alpha*m*firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^(i)*(secondpart(lambda,tau,t,alpha,beta))^(-i+m+1))/beta)
-      p <- p + p_temp
-    }
-  }
-  p
-   
-}
-
-
-
 firstpart <- function(t,tau,beta,alpha){
   ((t-tau)/beta)^(alpha-1)
 }
@@ -83,19 +53,48 @@ secondpart <- function(lambda,tau,t,alpha,beta){
   exp(-lambda*tau-((t-tau)/beta)^alpha)
 }
 
+pdf_R_g <- function(j,L,W,t,tau,lambda,alpha,beta){
+  
+  p <- 0
+  m <- L*W
+  
+  if(t <= tau) {
+    for(i in 0:j) {
+      
+      p_temp <- faults(i,L,W) * -((i*lambda*((1-exp(-t*lambda))^(i-1))*exp(lambda*t*(-(m-i))-lambda*t)) - (lambda*(m-i)*((1-exp(-t*lambda))^i) * exp(lambda*t*(-(m-i)))))
+      
+      p <- p + p_temp
+    }
+    
+  } else {
+    for(i in 0:j) {
+      
+      p_temp <- faults(i,L,W) * ((alpha*(m-i) * firstpart(t,tau,beta,alpha)*(1-secondpart(lambda,tau,t,alpha,beta))^i *((secondpart(lambda,tau,t,alpha,beta)^(m-i))))/beta - 
+                                   (alpha* i * firstpart(t,tau,beta,alpha)*(1-(secondpart(lambda,tau,t,alpha,beta)))^(i-1) *((secondpart(lambda,tau,t,alpha,beta)^(m-i+1))))/beta)
+      p <- p + p_temp
+    }
+  }
+  p
+  
+}
+
+
+
+#### Trials with functions ####
 
 
 
 
 pdf_R_g(2,4,2,10,20,.0024,10,10)
 
-alpha <- 5
-beta <- 55
-tau <- 20
 lambda <- .0024
-L <- 400
+alpha <- 1
+beta <- .25*1/lambda
+tau <- 20
+
+L <- 128
 W <- 2
-j <- 200
+j <- 128
 
 df <- data.frame() 
 
@@ -112,6 +111,14 @@ for(i in seq(from =1,to =40, by = .1)) {
 ggplot(df, aes(x = t, y = f_t)) +
   geom_path() +
   geom_path(aes(y = 1-R_g),color = "red")
+
+
+
+dftest <- df %>%
+  
+  mutate(int = .1*f_t)
+
+sum(dftest$int) # Should integrate to R_g(max(t))
 
 alp <- 3
 bet <- 20
